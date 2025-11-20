cbe2545ffbf2aa3882a95beed83bd83c891efbe3
Josiah Aviso <150400184+JoshAviso@users.noreply.github.com>
2025-11-20 16:15:09 +0800

    initial commit

---
+++ P4_Scene_Streaming.cpp
@@ -0,0 +1,12 @@
+﻿// P4_Scene_Streaming.cpp : Defines the entry point for the application.
+//
+
+#include "P4_Scene_Streaming.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ P4_Scene_Streaming.h
@@ -0,0 +1,8 @@
+﻿// P4_Scene_Streaming.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
