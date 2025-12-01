0a92ffb0a4f63ef4f935c3819b8ca27f14643837
Josiah Aviso <150400184+JoshAviso@users.noreply.github.com>
2025-12-01 08:44:41 +0800

    scene select looks better now

--- src_common/Public/UserInterface/CustomUI/SceneSelectUI.h
+++ src_common/Public/UserInterface/CustomUI/SceneSelectUI.h
@@ -7,14 +7,26 @@ class SceneSelectUI : public IUserInterface {
 public:
 	SceneSelectUI() : IUserInterface("Scene Select") {};
 private:
+	Shared<Scene> _selectedScene = nullptr;
 	void Render() override {
 		if (ImGui::Begin("Select Scene")) {
 			List<Shared<Scene>> scenes = SceneManager::GetScenes();
+
+			ImVec2 button_dim = ImVec2(ImGui::GetContentRegionAvail().x, 0.f);
+
 			for (int i = 0; i < scenes.size(); i++) {
 				Shared<Scene> scene = scenes[i];
-				if (ImGui::Button(scene->GetName().c_str())) {
+				bool selected = scene == _selectedScene;
+				if (selected)
+					ImGui::PushStyleVar(ImGuiStyleVar_Alpha, 0.5f);
+
+				if (ImGui::Button(scene->GetName().c_str(), button_dim)) {
+					_selectedScene = scene;
 					SceneManager::OpenScene(scene->GetName());
 				}
+
+				if (selected)
+					ImGui::PopStyleVar();
 			}
 
 			ImGui::End();
