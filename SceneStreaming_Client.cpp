a7da3e60afbfcd3ba84e133c38e9551d88ef39c8
Josiah Aviso <150400184+JoshAviso@users.noreply.github.com>
2025-12-04 20:48:47 +0800

    server and client programs should be ready once grpc stuff is good

--- SceneStreaming_Client.cpp
+++ SceneStreaming_Client.cpp
@@ -1,60 +1,43 @@
 ﻿
 #include "SceneStreaming_Client.h"
 
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <imgui.h>
-#include <backends/imgui_impl_glfw.h>
-#include <backends/imgui_impl_opengl3.h>
-#include <tiny_obj_loader.h>
-
-#include <iostream>
-#include <vector>
-
 #include <Application.h>
 #include <Window/Window.h>
 
-#include <Resources/ResourceManager.h>
-#include <Resources/Mesh.h>
-#include <Resources/IShaderResource.h>
 #include <Objects/ObjectManager.h>
 #include <Objects/Object.h>
-#include <Graphics/ShaderList.h>
-#include <Components/Renderers/MeshRenderer.h>
 #include <Components/Update/CameraComponent.h>
 #include <Components/Update/CameraController.h>
 
 #include <UserInterface/UserInterfaceManager.h>
-#include <UserInterface/CustomUI/DemoUI.h>
 #include <UserInterface/CustomUI/SceneSelectUI.h>
 
 int main()
 {
+    // Initialize Client Stuff
+	//RunClient("Frankling", "localhost:50051", true);
+
     Application* app = Application::Initialize({
         120.f,
         new Window({
-                "P4 Scene Streaming | Aviso & Taylan",
+                "P4 Scene Streaming Client | Aviso & Taylan",
                 800, 600,
                 Color::Black()
             }),
         SceneManager::Desc{
             true, "MainCamera"
         }
-        });
-
+    });
 
     // Camera Setup
     Object* cam = ObjectManager::RegisterObject(new Object("MainCamera"));
-    cam->AddComponent(new CameraComponent("MainCam",
-        new Camera::PerspectiveProjection(45.f, Application::GetWindow()->AspectRatio(), 0.01, 200.f)));
+    cam->AddComponent(new CameraComponent("MainCam", new Camera::PerspectiveProjection(45.f, Application::GetWindow()->AspectRatio(), 0.01, 200.f)));
     cam->AddComponent(new CameraController(1.f, 360.f));
     cam->transform.position = Vec3(0.f, 0.f, 1.0f);
 
-    //UserInterfaceManager::AddUI(new DemoUI());
     UserInterfaceManager::AddUI(new SceneSelectUI());
 
     Application::Run();
-	//RunClient("Frankling", "localhost:50051", true);
 
     return 0;
 }
--- SceneStreaming_Server.cpp
+++ SceneStreaming_Server.cpp
@@ -1,8 +1,43 @@
 ﻿
 #include "SceneStreaming_Server.h"
 
+#include <Application.h>
+#include <Window/Window.h>
+
+#include <Objects/ObjectManager.h>
+#include <Objects/Object.h>
+#include <Components/Update/CameraComponent.h>
+#include <Components/Update/CameraController.h>
+
+#include <UserInterface/UserInterfaceManager.h>
+#include <UserInterface/CustomUI/SceneSelectUI.h>
+
 int main()
 {
+	// Initialize Server
 	RunServer("0.0.0.0:50051");
+
+    Application* app = Application::Initialize({
+        120.f,
+        new Window({
+                "P4 Scene Streaming Server | Aviso & Taylan",
+                800, 600,
+                Color::Black()
+            }),
+        SceneManager::Desc{
+            true, "MainCamera"
+        }
+        });
+
+    // Camera Setup
+    Object* cam = ObjectManager::RegisterObject(new Object("MainCamera"));
+    cam->AddComponent(new CameraComponent("MainCam", new Camera::PerspectiveProjection(45.f, Application::GetWindow()->AspectRatio(), 0.01, 200.f)));
+    cam->AddComponent(new CameraController(1.f, 360.f));
+    cam->transform.position = Vec3(0.f, 0.f, 1.0f);
+
+    UserInterfaceManager::AddUI(new SceneSelectUI());
+
+    Application::Run();
+
 	return 0;
 }
